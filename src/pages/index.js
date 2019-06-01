caa3341a5272eacaec62c066ee2b0d1d1aa06181
Billy Bunn <williammbunn@gmail.com>
2019-05-31 19:21:53 -0700

    creates pages from md

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
 
 import Layout from "../components/layout"
-import Header from "../components/header"
 
 export default ({ data }) => {
-  console.log(data)
   return (
     <Layout>
       <div>
@@ -20,21 +20,29 @@ export default ({ data }) => {
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <h3
+            <Link
+              to={node.fields.slug}
               css={css`
-                margin-bottom: ${rhythm(1 / 4)};
+                text-decoration: none;
+                color: inherit;
               `}
             >
-              {node.frontmatter.title}{" "}
-              <span
+              <h3
                 css={css`
-                  color: #bbb;
+                  margin-bottom: ${rhythm(1 / 4)};
                 `}
               >
-                — {node.frontmatter.date}
-              </span>
-            </h3>
-            <p>{node.excerpt}</p>
+                {node.frontmatter.title}{" "}
+                <span
+                  css={css`
+                    color: #bbb;
+                  `}
+                >
+                  — {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </Link>
           </div>
         ))}
       </div>
@@ -44,17 +52,19 @@ export default ({ data }) => {
 
 export const query = graphql`
   query {
-    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
       edges {
         node {
+          id
           frontmatter {
             title
-            date
+            date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
-          rawMarkdownBody
-          html
-          htmlAst
         }
       }
     }
